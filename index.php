930f4ef68f89eecbf73b234d275e3ecdc930be06
MatteoBassanin <matteo.bassanin@gmail.com>
2023-04-17 15:50:35 +0200

    forgot label

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
 
     <form method="GET" action="censor.php">
         <div class="container">
-            <label for="censor"></label>
+            <label for="censor">Parola da censurare</label>
             <input type="text" placeholder="parola da censurare" id="censor" name="myCensor"/>
         </div>
         <div class="container" >
-            <label for="my_text"></label>
+            <label for="my_text">Testo da censurare</label>
             <input type="text" placeholder="inserisci il tuo testo" id="my_text" name="mySentence"/>
         </div>
         <button type="submit">Invia</button>
